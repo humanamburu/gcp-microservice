46734572f588cffc495199db7740126adfb100a5
humanamburu <humanamburu@gmail.com>
2020-03-27 12:28:39 +0300

    feat: add pg database

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import redis from 'redis';
+import { Pool } from 'pg';
 
 const {
   PORT = 3000,
   REDIS_PORT = 6379,
-  REDIS_HOST = 'redis-server'
+  REDIS_HOST = 'redis-server',
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST
 } = process.env;
 
 const app = express();
@@ -12,16 +16,23 @@ const rc = redis.createClient({
   host: REDIS_HOST,
   port: Number(REDIS_PORT)
 });
+const bdPool = new Pool({
+  user: DB_USER,
+  host: DB_HOST,
+  password: DB_PASSWORD,
+});
 
-rc.set('visits', String(0));
+// tslint:disable-next-line:no-console
+bdPool.on('error', (e) => console.log('POSTGRES ERROR', e));
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  rc.get('visits', ((err, reply) => {
-    const visits = Number(reply);
+  rc.get('visits', async (err, reply) => {
+    const visits = err ? 0 : Number(reply);
+    const { rows: [{ now }] } = await bdPool.query('SELECT NOW()');
 
-    res.send(`Hello guest number #${reply}!`);
+    res.send(`[${now}]: Hello guest number #${reply}!`);
     rc.set('visits', String(visits + 1))
-  }));
+  });
 });
 
 app.listen(PORT, () => {
